518c99092e41ea8723547ba1389b5182b29fad70
leonardoedgar <leonardo_edgar98@outlook.com>
2020-11-21 01:12:47 +0800

    update example script to communicate with arduino

--- inspection/scripts/communication/communicate_with_arduino.py
+++ inspection/scripts/communication/communicate_with_arduino.py
@@ -5,11 +5,11 @@ import os
 
 if __name__ == '__main__':
     arduino = SerialDevice(port=os.getenv("SERIAL_DEVICE_PORT"), baud_rate=9600)
-    data_to_send = 1
     while True:
         try:
-            print("Received arduino data: ", arduino.get_request())
-            arduino.send_response(data_to_send)
+            data_to_send = arduino.get_request()
+            print("Received arduino data: ", data_to_send)
             data_to_send += 1
+            arduino.send_response(data_to_send)
         except KeyboardInterrupt:
             break
